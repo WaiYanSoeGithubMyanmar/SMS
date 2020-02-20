a0834b6ce3e3666f3de12c26580988cf023a5b7f
WaiYanMoung <geminegmr@gmail.com>
2020-02-20 16:08:00 +0630

    Seeding for attendance types.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,46 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $items = [          
+        [
+            'type' => 'Absent',
+            'key_value' => '<b class="text-danger">A<b>',
+            'is_active' => 'yes',
+            'domain' => 'all',
+            'session_id' => '1',
+        ],
+        [
+            'type' => 'Half',
+            'key_value' => '<b class="text-warning">F<b>',
+            'is_active' => 'yes',
+            'domain' => 'all',
+            'session_id' => '1'
+        ],
+        [
+            'type' => 'Holiday',
+            'key_value' => '<b class="text-danger">A<b>',
+            'is_active' => 'no',
+            'domain' => 'all',
+            'session_id' => '1'
+        ],
+        [
+            'type' => 'Absent',
+            'key_value' => '<b class="text-danger">A<b>',
+            'is_active' => 'yes',
+            'domain' => 'all',
+            'session_id' => '1'
+        ]
+        ];
+        foreach ($items as $item) {
+            DB::table('attendance_types')->insert([
+                'type' => $item['type'],
+                'key_value' => $item['key_value'],
+                'is_active' => $item['is_active'],
+                'domain' => $item['domain'],
+                'session_id'=> $item['session_id']
+            ]);
+        }
+        
+
     }
 }
